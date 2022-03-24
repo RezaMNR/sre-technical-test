b5b35b2543d1d5130c3603b2add93ab70e01839c
Reza MNR <muhammad@hostinger.com>
2022-03-24 08:05:56 +0700

    add script soure

--- chef-repo/cookbooks/chef_docker_jenkins/recipes/default.rb
+++ chef-repo/cookbooks/chef_docker_jenkins/recipes/default.rb
@@ -4,6 +4,25 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 docker_service 'default' do
-    group 'dockerroot'
-    action [:create, :start]
-  end
+  group 'dockerroot'
+  action [:create, :start]
+end
+
+# Pull official Jenkins image
+docker_image 'jenkinsci/jenkins' do
+  tag 'lts'
+  action :pull
+end
+
+docker_volume 'jenkins-data' do
+  action :create
+end
+
+# Run container
+docker_container 'jenkins' do
+  repo 'jenkinsci/jenkins'
+  tag 'lts'
+  action :run
+  port '8080:8080'
+  volumes 'jenkins-data:/var/jenkins_home/'
+end
